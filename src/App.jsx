45e86910b827d39628cbe20c8e3942239321fa16
nbogie <neillbogie@googlemail.com>
2024-06-26 09:20:05 +0000

    mostly formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import React from "react"
-import './App.css'
-import TileData from "./TileData"
-import Tile from "./Tile"
+import React from "react";
+import "./App.css";
+import TileData from "./TileData";
+import Tile from "./Tile";
 
 function App() {
-  const [tiles, setTiles] = React.useState(TileData)
-  
-  const tileElements = tiles.map(tile => (
-      <Tile 
-      key={tile.id} 
-      sideStatus={tile.sideStatus}
-      emoji={tile.emojiSide}
-      word={tile.wordSide} 
-      />
-  ))
-  
-  return (
-      <main>
-          <h1>What is that called?</h1>
-          {tileElements}
-      </main>
-  )
+    const [tiles, setTiles] = React.useState(TileData);
+
+    const tileElements = tiles.map((tile) => (
+        <Tile
+            key={tile.id}
+            sideStatus={tile.sideStatus}
+            emoji={tile.emojiSide}
+            word={tile.wordSide}
+        />
+    ));
+
+    return (
+        <main>
+            <h1>What is that called?</h1>
+            {tileElements}
+        </main>
+    );
 }
 
-export default App
+export default App;
--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default function Tile(props) {
-  const [sideStatus, setSideStatus] = React.useState(props.sideStatus);
+    const [sideStatus, setSideStatus] = React.useState(props.sideStatus);
 
-  const side = sideStatus ? props.emoji : props.word;
+    const side = sideStatus ? props.emoji : props.word;
 
-  function toggle() {
-    setSideStatus((prevSide) => !prevSide);
-  }
+    function toggle() {
+        setSideStatus((prevSide) => !prevSide);
+    }
 
-  return (
-    <div className="tile" key={props.id} onClick={toggle}>
-      <p className="text">{side}</p>
-    </div>
-  );
+    return (
+        <div className="tile" key={props.id} onClick={toggle}>
+            <p className="text">{side}</p>
+        </div>
+    );
 }
